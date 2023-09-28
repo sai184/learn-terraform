fe94e04f98d25306951a1672945959f84aa74b50
saina <sainagarjuna184@gmail.com>
2023-09-28 19:39:10 +0530

    rre

---
+++ 04-provisioner/main.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "test" {
+  provisioner "local-exec" {
+    command = "echo Hello World"
+  }
+}
+
+
+resource "null_resource" "test1" {
+  provisioner "local-exec" {
+    command = <<EOF
+echo Hello World
+ls
+pwd
+EOF
+  }
+}
