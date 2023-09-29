3913a399c6c73fdbfd1d15d7930ff0048abe2301
saina <sainagarjuna184@gmail.com>
2023-09-29 20:51:10 +0530

    rre

--- Expense-v3/main.tf
+++ Expense-v3/main.tf
@@ -1,23 +1,8 @@
-
-
 module "expense" {
   source = "./appcreate"
   count = var.expense[count.index]
 }
 variable "expense" {
-  deafult = ["frontend","backend","mysql"]
+  default = ["frontend","backend","mysql"]
 }
-=======
-module "frontendt" {
-  source = "appcreate"
-  component = "frontend"
-}
-module "backendt" {
-  source = "appcreate"
-  component = "backend"
-}
-module "mysqlt" {
-  source = "appcreate"
-  component = "mysql"
-}
->>>>>>> origin/main
+
