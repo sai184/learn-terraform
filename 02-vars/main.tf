e77be4637329642dab25ad13b922a4ef5f36638c
saina <sainagarjuna184@gmail.com>
2023-09-30 16:02:15 +0530

    rre

---
+++ 02-vars/main.tf
@@ -0,0 +1,28 @@
+variable "fruit" {
+  default = "apple"
+}
+
+variable "fruits" {
+  default = ["apple", "banana"]
+}
+
+variable "fruit_price" {
+  default = {
+    apple  = 10
+    banana = 20
+  }
+}
+
+output "fruit" {
+  value = var.fruit
+}
+
+output "first_fruit" {
+  value = var.fruits[0]
+}
+
+output "fruit_price" {
+  value = "Price of apple = ${var.fruit_price["apple"]}"
+}
+
+variable "url" {}
